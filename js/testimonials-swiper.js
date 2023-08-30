c25ddcc5e23c4c68ac60ef7ec5f1556aad38d0fb
Vitalii Prokopenko <doc8259@gmail.com>
2023-08-30 21:39:39 +0300

    testimonials styles added

---
+++ js/testimonials-swiper.js
@@ -0,0 +1,47 @@
+const swiper = new Swiper('.testimonials-slider', {
+    // Classes
+    // slidePrevClass: '.hero-prev-slide',
+    // slideNextClass: '.hero-next-slide',
+  
+    // Optional parameters
+  
+    direction: 'horizontal',
+    loop: true,
+    centeredSlides: false,
+    effect: 'coverflow',
+    grabCursor: true,
+    speed: 1000,
+    slidesPerView: 'auto',
+    spaceBetween: 30,
+  
+    autoplay: {
+      delay: 4000,
+      disableOnInteraction: false,
+    },
+  
+    coverflowEffect: {
+      rotate: 0,
+      slideShadows: false,
+      scale: 1,
+      depth: 0,
+    },
+  
+    // If we need pagination
+    pagination: {
+      el: '.testimonials-pagination',    
+      bulletClass: 'testimonials-pagination-bullet',
+      bulletActiveClass: 'testimonials-pagination-bullet-active',
+    },
+  
+    // Navigation arrows
+    // navigation: {
+    //   nextEl: '.swiper-button-next',
+    //   prevEl: '.swiper-button-prev',
+    // },
+  
+    // And if we need scrollbar
+    // scrollbar: {
+    //   el: '.swiper-scrollbar',
+    // },
+  });
+  
