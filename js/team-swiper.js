471648eecdd69b2d65f7f036d69568c1b8a806b0
Vitalii Prokopenko <doc8259@gmail.com>
2023-08-31 22:07:27 +0300

    adds styles for sliders and others

--- js/team-swiper.js
+++ js/team-swiper.js
@@ -1,12 +1,11 @@
 const swiper = new Swiper('.team-slider', {
-// Classes
-    wrapperClass: 'team-slider-wrapper',
-    slideClass: 'team-slide',
-    slideActiveClass: 'active-slide',
-    slidePrevClass: 'prev-slide',
-    slideNextClass: 'next-slide',
-    slideVisibleClass: 'visible-slide',
-    
+  // Classes
+  wrapperClass: 'team-slider-wrapper',
+  slideClass: 'team-slide',
+  slideActiveClass: 'active-slide',
+  slidePrevClass: 'prev-slide',
+  slideNextClass: 'next-slide',
+  slideVisibleClass: 'visible-slide',
 
   // Optional parameters
 
@@ -17,19 +16,19 @@ const swiper = new Swiper('.team-slider', {
   grabCursor: true,
   speed: 1000,
   slidesPerView: 3,
-  spaceBetween: 20,
+  spaceBetween: 29,
 
   autoplay: {
-    delay: 4000,
+    delay: 5000,
     disableOnInteraction: false,
   },
 
-//   
+  //
 
   // If we need pagination
-//   pagination: {
-//     el: '.swiper-pagination',
-//   },
+  //   pagination: {
+  //     el: '.swiper-pagination',
+  //   },
 
   // Navigation arrows
   navigation: {
@@ -43,6 +42,23 @@ const swiper = new Swiper('.team-slider', {
   // },
 });
 
+let clickedSlide;
+let clickedSlideOverlay;
+
 swiper.on('click', function () {
   console.log('slide clicked');
+
+  clickedSlide = this.clickedSlide;
+  clickedSlide.style.transform = 'scale(1.2857)';
+  clickedSlide.style.zIndex = '999';
+  clickedSlideOverlay = clickedSlide.lastElementChild;
+  clickedSlideOverlay.style.transform = 'translateY(0%)';
+});
+
+swiper.on('slideChange', function () {
+  console.log('slide changed');
+  if (clickedSlide) {
+    clickedSlide.style.transform = 'scale(1)';
+    clickedSlideOverlay.style.transform = 'translateY(100%)';
+  }
 });
